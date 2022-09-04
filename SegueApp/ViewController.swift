78168d561f2cff29b129440ff24890fb5c34ccd2
Serkan BOYRACI <serkanboyraci@gmail.com>
2022-09-04 08:56:44 +0300

    Initial Commit

---
+++ SegueApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegueApp
+//
+//  Created by Ali serkan Boyracı  on 4.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
