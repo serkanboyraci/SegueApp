e94d901aa26e354a272ee327322317e923335112
serkanboyraci <serkanboyraci@gmail.com>
2023-09-11 09:53:18 +0200

    Update SecondVC.swift

--- SegueApp/SecondVC.swift
+++ SegueApp/SecondVC.swift
@@ -12,12 +12,14 @@ class SecondVC: UIViewController {
     
     @IBOutlet var nameLabel: UILabel!
     
-    var myName = " "
+    var myName = " " //ekran acildiginda gosterilesi ve 1VC den ulasilabilmesi amaciyla class icinde myName tanimladik.
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //ekran acildiginda labelda gosterilecek text e myName i esitledik.
         nameLabel.text = myName
+        
         
     }
  
